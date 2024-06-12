92b26b74a43ec572416226e161c3c172e560c9ad
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-06-12 19:52:50 -0300

    add signup client form

--- front-end-booking/src/app/app.module.ts
+++ front-end-booking/src/app/app.module.ts
@@ -1,27 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './basic/components/login/login.component';
 import { SignupComponent } from './basic/components/signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SignupClientComponent } from './basic/components/signup-client/signup-client.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    SignupClientComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     MatToolbarModule,
     MatButtonModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
     ReactiveFormsModule
   ],
   providers: [
---
+++ front-end-booking/src/app/basic/components/signup-client/signup-client.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup-client',
+  templateUrl: './signup-client.component.html',
+  styleUrl: './signup-client.component.scss'
+})
+export class SignupClientComponent {
+  validateForm!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private notification: MatSnackBar,
+    private router: Router) {}
+
+    ngOnInit() {
+      this.validateForm = this.fb.group({
+        email: [null, [Validators.email, Validators.required]],
+        name: [null, [Validators.required]],
+        lastname: [null, [Validators.required]],
+        phone: [null],
+        password: [null, [Validators.required]],
+        checkPassword: [null, [Validators.required]],
+      })
+    }
+
+    submitForm() {
+      this.authService.registerClient(this.validateForm.value).subscribe(res => {
+        this.notification.open('Signup successful');
+        this.router.navigateByUrl('/login');
+      }, error => {
+        this.notification.open('Error');
+      });
+    }
+}
